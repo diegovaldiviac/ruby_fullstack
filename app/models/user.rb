d1db2e872265c8de654e66816ba336002412b88b
diegovaldiviac <dvaldiviac@buk.cl>
2024-05-27 14:34:03 -0400

    deleted devise gem and implemented personal auth tracking

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,27 +1,4 @@
-# == Schema Information
-#
-# Table name: user
-#
-#  name                                    :string           not null
-#  password                                :string           not null
-#  email                                   :string           not null
-#  created_at                              :datetime         not null
-#  updated_at                              :datetime         not null
-#  reset_password_token                    :string
-#  encrypted_password                      :string
-#  reset_password_sent_at                  :datetime
-#  remember_created_at                     :datetime
-
-# Indexes
-#
-#  index_users_on_email                     (email) UNIQUE
-#  index_users_on_reset_password_token      (reset_password_token) UNIQUE
-
 class User < ApplicationRecord
-    # Include default devise modules. Others available are:
-    # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-    devise :database_authenticatable, :registerable,
-            :recoverable, :rememberable, :validatable
-    #has_paper_trail ignore: [:encrypted_password, :reset_password_token, :updated_at], meta: nil
-  
+
+    has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "home#index"
+
+  get 'signup', to: 'users#new' 
+  resources :users
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy' 
 end
